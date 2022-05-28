9440da6284cae70a8994a2a1f0dcb8d0ec273409
Prabesh <prabesh.bista369@gmail.com>
2022-05-28 20:10:25 +0545

    Ui #1 completed

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,10 +8,35 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
      Size size=MediaQuery.of(context).size;
     return Scaffold(
-      body: SizedBox(
-        width:size.width,
-        height: size.height,
+      body: CustomScrollView(
+    slivers: <Widget>[
+      //2
+      SliverAppBar(
+        pinned: true,
+        backgroundColor: Colors.red,
+        expandedHeight: size.height*0.2,
+        flexibleSpace: const FlexibleSpaceBar(
+          title: Text("Category List", textScaleFactor: 1.5),
+            
+        ),
       ),
+      //3
+      SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (_, int index) {
+            return ListTile(
+              leading: Container(
+                  padding: EdgeInsets.all(8),
+                  width: 100,
+                  child: Placeholder()),
+              title: Text('Place ${index + 1}', textScaleFactor: 2),
+            );
+          },
+          childCount: 20,
+        ),
+      ),
+    ],
+  ),
     );
   }
 }
